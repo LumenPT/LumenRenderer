6f2fe658a907244ed043debd188728acabf6edce
AlphaAlexRed <44769712+AlphaAlexRed@users.noreply.github.com>
2020-12-03 10:23:51 +0100

    [+] Add CMake configuration for LumenPT render module project

--- Lumen_Engine/Sandbox/src/Application.cpp
+++ Lumen_Engine/Sandbox/src/Application.cpp
@@ -1,8 +1,22 @@
 #include <Lumen.h>
+#include "Glad/include/glad/glad.h"
 
 #include "imgui/imgui.h"
 
+#include "GLFW/include/GLFW/glfw3.h"
+
+#include "LumenPT.h"
+
 //#include "imgui/imgui.h"
+
+namespace Lumen {
+	class WindowsWindow;
+}
+
+namespace LumenPTConsts
+{
+	const static std::string ShaderPath = "whatever";
+}
 
 class ExampleLayer : public Lumen::Layer
 {
@@ -10,12 +24,12 @@ public:
 	ExampleLayer()
 		: Layer("Example")
 	{
-		
+
 	}
 
 	void OnUpdate() override
 	{
-		if(Lumen::Input::IsKeyPressed(LMN_KEY_TAB))
+		if (Lumen::Input::IsKeyPressed(LMN_KEY_TAB))
 			LMN_INFO("Tab  key is pressed");
 	}
 
@@ -29,7 +43,7 @@ public:
 	void OnEvent(Lumen::Event& event) override
 	{
 
-		if(event.GetEventType() == Lumen::EventType::KeyPressed)
+		if (event.GetEventType() == Lumen::EventType::KeyPressed)
 		{
 			Lumen::KeyPressedEvent& e = static_cast<Lumen::KeyPressedEvent&>(event);
 			LMN_TRACE("{0}", static_cast<char>(e.GetKeyCode()));
@@ -43,13 +57,27 @@ class Sandbox : public Lumen::LumenApp
 public:
 	Sandbox()
 	{
+		glfwMakeContextCurrent(reinterpret_cast<GLFWwindow*>(GetWindow().GetNativeWindow()));
+
+		/*OptiXRenderer::InitializationData init;
+
+		LumenPT::InitializationData lptInit;
+
+		OptiXRenderer optx(init);
+
+		LumenPT lpt(init);*/
+
+		uint32_t b;
+		glGenBuffers(1, &b);
+		auto err = glGetError();
+
 		PushLayer(new ExampleLayer());
 		//PushOverlay(new Lumen::ImGuiLayer());
 	}
 
 	~Sandbox()
 	{
-		
+
 	}
 };
 
