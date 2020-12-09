1db93456010e3f17944374220e671f8b5301764b
AlphaAlexRed <44769712+AlphaAlexRed@users.noreply.github.com>
2020-12-09 10:15:15 +0100

    [*] Project now uses compiled .ptx files.

--- Lumen_Engine/LumenPT/src/Shaders/CppCommon/LumenPTConsts.h
+++ Lumen_Engine/LumenPT/src/Shaders/CppCommon/LumenPTConsts.h
@@ -5,5 +5,6 @@
 
 namespace LumenPTConsts
 {
-	const std::string gs_ShaderPathBase = std::string(std::string(LUMENPT_DIRECTORY) + "\\Assets\\PrecompiledShaders\\Debug\\");
+	//const std::string gs_ShaderPathBase = std::string(std::string(LUMENPT_DIRECTORY) + "\\Assets\\PrecompiledShaders\\Debug\\");
+	const std::string gs_ShaderPathBase = std::string(std::string(LUMENPT_BINARIES_DIRECTORY) + "\\LumenPT\\CudaPTX.dir\\Debug\\");
 }
