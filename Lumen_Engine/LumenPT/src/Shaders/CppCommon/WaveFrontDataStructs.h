8c1fe58f2139639edd8a58ae7fcf3cdefb0350f2
Bas Walhout <bas123456789bas@hotmail.nl>
2021-01-06 11:21:56 +0100

    Add Wave Front Data structures.
    [*]WaveFrontDataStructs.h: Add simplistic versions of Datastructures necessary in shaders, kernels and Cpp for the Wave Front algorithm

--- Lumen_Engine/LumenPT/src/Shaders/CppCommon/WaveFrontDataStructs.h
+++ Lumen_Engine/LumenPT/src/Shaders/CppCommon/WaveFrontDataStructs.h
@@ -1,2 +1,109 @@
 #pragma once
 
+#include <Cuda/cuda/helpers.h>
+
+/*TODO: check if usage of textures can benefit performance
+ *for buffers like intersectionBuffer or OutputBuffer.
+*/
+
+struct RayData
+{
+    
+    const float3 m_Origin;
+    const float3 m_Direction;
+    const float3 m_Contribution;
+
+};
+
+struct RayBatch
+{
+
+    const unsigned int m_Size;
+    RayData m_Rays[];
+
+};
+
+struct ShadowRayData
+{
+
+    const float3 m_Origin;
+    const float3 m_Direction;
+    const float m_MaxDistance;
+    const float3 m_PotentialRadiance;
+    unsigned int m_OutputChannelIndex;
+
+};
+
+struct ShadowRayBatch
+{
+
+    const unsigned int m_Size;
+    ShadowRayData m_ShadowRays[];
+
+};
+
+struct IntersectionData
+{
+
+    unsigned int m_RayId;
+    float m_IntersectionT;
+    unsigned int m_TriangleId;
+    unsigned int m_MeshId;
+
+};
+
+struct IntersectionBuffer
+{
+
+    const unsigned int m_Size;
+    IntersectionData m_Intersections[];
+
+};
+
+struct OutputBuffer
+{
+
+    const static unsigned int m_NumOutputChannels = 3;
+    const unsigned int m_Size;
+    float3 m_Pixels[][m_NumOutputChannels];
+
+};
+
+struct PixelBuffer
+{
+
+    RayBatch* m_PrimaryRays;
+    IntersectionBuffer* m_PrimaryIntersections;
+    OutputBuffer* m_PixelOutput;
+
+};
+
+struct ShadingLaunchParameters
+{
+
+    PixelBuffer* m_Output;
+    PixelBuffer* m_PrevOutput;
+    RayBatch* m_SecondaryRays;
+    IntersectionBuffer* m_Intersections;
+    const float2 m_OutputSize;
+    ShadowRayBatch* m_ShadowRaysBatches[];
+
+};
+
+struct DeviceCameraData
+{
+
+    float3 m_Position;
+    float3 m_Up;
+    float3 m_Right;
+    float3 m_Forward;
+
+};
+
+struct PrimRayGenLaunchParameters
+{
+
+    DeviceCameraData m_Camera;
+    float2 m_OutputSize;
+
+};
