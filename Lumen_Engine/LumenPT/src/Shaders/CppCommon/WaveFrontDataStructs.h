0487796970cbf2b0a88fb7e85dd416acef108faa
Bas Walhout <bas123456789bas@hotmail.nl>
2021-01-12 15:28:47 +0100

    [*]Removed const from some datastruct members to allow use in arrays.

--- Lumen_Engine/LumenPT/src/Shaders/CppCommon/WaveFrontDataStructs.h
+++ Lumen_Engine/LumenPT/src/Shaders/CppCommon/WaveFrontDataStructs.h
@@ -1,77 +1,13 @@
 #pragma once
 
+#include "Cuda/cuda.h"
+
 /*TODO: check if usage of textures can benefit performance
  *for buffers like intersectionBuffer or OutputBuffer.
 */
 
 namespace WaveFront
 {
-
-    struct RayData
-    {
-
-        //Read only
-        const float3 m_Origin;
-        const float3 m_Direction;
-        const float3 m_Contribution;
-
-    };
-
-    struct RayBatch
-    {
-
-        //Read only
-        const unsigned int m_Size;
-
-        //Read/Write
-        RayData m_Rays[];
-
-    };
-
-    struct ShadowRayData
-    {
-
-        //Read only
-        const float3 m_Origin;
-        const float3 m_Direction;
-        const float m_MaxDistance;
-        const float3 m_PotentialRadiance;
-        const unsigned int m_OutputChannelIndex;
-
-    };
-
-    struct ShadowRayBatch
-    {
-
-        //Read only
-        const unsigned int m_Size;
-
-        //Read/Write
-        ShadowRayData m_ShadowRays[];
-
-    };
-
-    struct IntersectionData
-    {
-
-        //Read only
-        const unsigned int m_RayId;
-        const float m_IntersectionT;
-        const unsigned int m_TriangleId;
-        const unsigned int m_MeshId;
-
-    };
-
-    struct IntersectionBuffer
-    {
-
-        //Read only
-        const unsigned int m_Size;
-
-        //Read/Write
-        IntersectionData m_Intersections[];
-
-    };
 
     struct PixelBuffer
     {
@@ -84,6 +20,8 @@ namespace WaveFront
             NUM_CHANNELS
         };
 
+        __global__ PixelBuffer(unsigned int a_Size);
+
         //Ready only
         constexpr static unsigned int m_NumOutputChannels = static_cast<unsigned>(OutputChannel::NUM_CHANNELS);
         const unsigned int m_Size;
@@ -93,8 +31,111 @@ namespace WaveFront
 
     };
 
+    struct RayData
+    {
+
+        RayData(
+            const float3& a_Origin, 
+            const float3& a_Direction, 
+            const float3& a_Contribution);
+
+        //Read only
+        float3 m_Origin;
+        float3 m_Direction;
+        float3 m_Contribution;
+
+    };
+
+    struct RayBatch
+    {
+
+        RayBatch(unsigned int a_Size);
+
+        //Read only
+        const unsigned int m_Size;
+
+        //Read/Write
+        RayData m_Rays[];
+
+        void AddRay(const RayData& a_data, unsigned int a_index)
+        {
+            if (a_index < m_Size)
+            {
+                m_Rays[a_index] = a_data;
+            }
+        }
+
+    };
+
+    struct ShadowRayData
+    {
+
+        ShadowRayData(
+            const float3& a_Origin,
+            const float3& a_Direction,
+            const float& a_MaxDistance,
+            const float3& a_PotentialRadiance,
+            PixelBuffer::OutputChannel a_OutputChannelIndex);
+
+        //Read only
+        float3 m_Origin;
+        float3 m_Direction;
+        float m_MaxDistance;
+        float3 m_PotentialRadiance;
+        PixelBuffer::OutputChannel m_OutputChannelIndex;
+
+    };
+
+    struct ShadowRayBatch
+    {
+
+        ShadowRayBatch(unsigned int a_Size);
+
+        //Read only
+        const unsigned int m_Size;
+
+        //Read/Write
+        ShadowRayData m_ShadowRays[];
+
+    };
+
+    struct IntersectionData
+    {
+
+        IntersectionData(
+            unsigned int a_RayId,
+            float a_IntersectionT,
+            unsigned int m_TriangleId,
+            unsigned int m_MeshId);
+
+        //Read only
+        unsigned int m_RayId;
+        float m_IntersectionT;
+        unsigned int m_TriangleId;
+        unsigned int m_MeshId;
+
+    };
+
+    struct IntersectionBuffer
+    {
+
+        IntersectionBuffer(unsigned int a_Size);
+
+        //Read only
+        const unsigned int m_Size;
+
+        //Read/Write
+        IntersectionData m_Intersections[];
+
+    };
+
     struct ResultBuffer
     {
+
+        ResultBuffer(
+            const RayBatch* a_PrimaryRays,
+            const IntersectionBuffer* a_PrimaryIntersections,
+            PixelBuffer* a_PixelOutput);
 
         //Read only
         const RayBatch* m_PrimaryRays;
@@ -125,7 +166,7 @@ namespace WaveFront
     struct SetupLaunchParameters
     {
 
-        SetupLaunchParameters();
+        SetupLaunchParameters(const uint2& a_Resolution, const DeviceCameraData& a_Camera);
         ~SetupLaunchParameters();
 
         const uint2 m_Resolution;
