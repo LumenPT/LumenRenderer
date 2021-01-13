b5be4d1125344999f265c261e6aef2d65f93f62e
Jan <janie177@hotmail.com>
2021-01-13 09:45:16 +0100

    Continue on pipeline implementation
    [+]
    Add camera ray generation.
    
    [*]
    Changed some function signatures to work with CUDA.

--- Lumen_Engine/LumenPT/src/Shaders/CppCommon/WaveFrontKernels.cuh
+++ Lumen_Engine/LumenPT/src/Shaders/CppCommon/WaveFrontKernels.cuh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "device_launch_parameters.h"
 #include <cuda_runtime.h>
 #include <cinttypes>
 #include "./WaveFrontDataStructs.h"
@@ -16,7 +17,7 @@ using namespace WaveFront;
  * Generates primary rays from the camera etc.
  * Rays are stored in the ray batch.
  */
-CPU void PreRenderSetup(const SetupLaunchParameters& a_SetupParams);
+CPU_ONLY void PreRenderSetup(const SetupLaunchParameters& a_SetupParams);
 
 /*
  * Call Optix to intersect all rays in the ray batch.
@@ -29,7 +30,7 @@ CPU_ONLY void IntersectRays();
  * This does direct, indirect and specular shading.
  * This fills the shadow ray buffer with potential contributions per pixel.
  */
-CPU void Shade(const ShadingLaunchParameters& a_ShadingParams);
+CPU_ONLY void Shade(const ShadingLaunchParameters& a_ShadingParams);
 
 /*
  * This resolves all shadow rays in parallel, and adds the light contribution
@@ -40,14 +41,14 @@ CPU_ONLY void ResolveShadowRays();
 /*
  * Apply de-noising, up scaling and post-processing effects.
  */
-CPU void PostProcess(const PostProcessLaunchParameters& a_PostProcessParams);
+CPU_ONLY void PostProcess(const PostProcessLaunchParameters& a_PostProcessParams);
 
 
 //The below functions are only called internally from the GPU within the above defined functions.
 
 //Called in setup.
-GPU void GenerateRays();
-GPU void GenerateMotionVectors();
+CPU_ONLY void GenerateRays(const SetupLaunchParameters& a_SetupParams);
+CPU_ONLY void GenerateMotionVectors();
 
 //Called during shading
 GPU void ShadeDirect();
@@ -59,3 +60,7 @@ GPU void Denoise();
 GPU void MergeLightChannels();
 GPU void DLSS();
 GPU void PostProcessingEffects();
+
+
+//Generate some rays based on the thread index.
+GPU void GenerateRay(int a_NumRays, RayData* a_Buffer, const float3& a_U, const float3& a_V, const float3& a_W, const float3& a_Eye, const int2& a_Dimensions);
--- Lumen_Engine/LumenPT/src/Shaders/WaveFrontKernels.cu
+++ Lumen_Engine/LumenPT/src/Shaders/WaveFrontKernels.cu
@@ -1,5 +1,7 @@
 #include "./CppCommon/WaveFrontKernels.cuh"
 
+#include "CppCommon/RenderingUtility.h"
+
 using namespace WaveFront;
 /*
  * Example calling order of functions.
@@ -30,31 +32,25 @@ using namespace WaveFront;
 
 
 
-CPU void Initialize(const std::uint32_t a_ScreenWidth, const std::uint32_t a_ScreenHeight, const std::uint32_t a_Depth)
-{
-    //TODO:
-    /*
-     * - Pixel buffer swap chain (once for current and one for previous frame). Each buffer contains the direct, indirect and specular light contribution per pixel (RGB). Primary ray also stored.
-     *
-     * - Ray batch for primary rays. Size is w * h * sizeof(PrimaryRay). Primary ray has direction, origin, potential contribution scalar.
-     *
-     * - Shadow ray batch size w * h * 2 * depth * sizeof(ShadowRay). Shadow ray has direction, origin, max distance, potential contribution, light channel index.
-     *
-     * - Intersection data size w * h * sizeof(IntersectionData). IntersectionData holds ID of the mesh and triangle for later lookup.
-     *
-     */
-}
+//void Initialize(const std::uint32_t a_ScreenWidth, const std::uint32_t a_ScreenHeight, const std::uint32_t a_Depth)
+//{
+//    //TODO:
+//    /*
+//     * - Pixel buffer swap chain (once for current and one for previous frame). Each buffer contains the direct, indirect and specular light contribution per pixel (RGB). Primary ray also stored.
+//     *
+//     * - Ray batch for primary rays. Size is w * h * sizeof(PrimaryRay). Primary ray has direction, origin, potential contribution scalar.
+//     *
+//     * - Shadow ray batch size w * h * 2 * depth * sizeof(ShadowRay). Shadow ray has direction, origin, max distance, potential contribution, light channel index.
+//     *
+//     * - Intersection data size w * h * sizeof(IntersectionData). IntersectionData holds ID of the mesh and triangle for later lookup.
+//     *
+//     */
+//}
 
-CPU void PreRenderSetup(const SetupLaunchParameters& a_SetupParams)
+CPU_ONLY void PreRenderSetup(const SetupLaunchParameters& a_SetupParams)
 {
-    //TODO
-    /*
-     * - Use the camera to generate rays. These rays 
-     * - Implement motion vectors
-     */
-
     //Generate rays based on the camera.
-    GenerateRays();
+    GenerateRays(a_SetupParams);
 
     //Generate motion vectors from the previous frame.
     GenerateMotionVectors();
@@ -68,7 +64,7 @@ CPU_ONLY void IntersectRays()
      */
 }
 
-CPU void Shade(const ShadingLaunchParameters& a_ShadingParams)
+CPU_ONLY void Shade(const ShadingLaunchParameters& a_ShadingParams)
 {
     //TODO
     /*
@@ -95,7 +91,7 @@ CPU_ONLY void ResolveShadowRays()
 }
 
 
-CPU void PostProcess(const PostProcessLaunchParameters& a_PostProcessParams)
+CPU_ONLY void PostProcess(const PostProcessLaunchParameters& a_PostProcessParams)
 {
     //TODO
     /*
@@ -106,4 +102,77 @@ CPU void PostProcess(const PostProcessLaunchParameters& a_PostProcessParams)
     MergeLightChannels();
     DLSS();
     PostProcessingEffects();
+}
+
+CPU_ONLY void GenerateRays(const SetupLaunchParameters& a_SetupParams)
+{
+    const float3 u = a_SetupParams.m_Camera.m_Up;
+    const float3 v = a_SetupParams.m_Camera.m_Right;
+    const float3 w = a_SetupParams.m_Camera.m_Forward;
+    const float3 eye = a_SetupParams.m_Camera.m_Position;
+    const int2 dimensions = make_int2(a_SetupParams.m_Resolution.x, a_SetupParams.m_Resolution.y);
+    const int numRays = dimensions.x * dimensions.y;
+
+    const int blockSize = 256;
+    const int numBlocks = (numRays + blockSize - 1) / blockSize;
+
+    //TODO pass buffer
+    GenerateRay<<<numBlocks, blockSize>>>(numRays, nullptr , u, v, w, eye, dimensions);
+}
+
+CPU_ONLY void GenerateMotionVectors()
+{
+}
+
+GPU void ShadeDirect()
+{
+}
+
+GPU void ShadeSpecular()
+{
+}
+
+GPU void ShadeIndirect()
+{
+}
+
+GPU void Denoise()
+{
+}
+
+GPU void MergeLightChannels()
+{
+}
+
+GPU void DLSS()
+{
+}
+
+GPU void PostProcessingEffects()
+{
+}
+
+GPU void GenerateRay(int a_NumRays, RayData* a_Buffer, const float3& a_U, const float3& a_V, const float3& a_W,
+                     const float3& a_Eye, const int2& a_Dimensions)
+{
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    for (int i = index; i < a_NumRays; i += stride)
+    {
+        //Convert the index into the screen dimensions.
+        const int screenY = i / a_Dimensions.x;
+        const int screenX = i - (screenY * a_Dimensions.x);
+
+        float3 direction = make_float3(static_cast<float>(screenX) / a_Dimensions.x,
+                                       static_cast<float>(screenY) / a_Dimensions.y, 0.f);
+        float3 origin = a_Eye;
+
+        direction.x = -(direction.x * 2.0f - 1.0f);
+        direction.y = -(direction.y * 2.0f - 1.0f);
+        direction = normalize(direction.x * a_U + direction.y * a_V + a_W);
+
+        RayData ray { origin, direction, make_float3(1.f, 1.f, 1.f) };
+        a_Buffer[i] = ray;
+    }
 }
