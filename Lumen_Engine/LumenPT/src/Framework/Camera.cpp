b0c1c1cac06db159d7f119a6cbf4f6ef0f0812da
Bas Walhout <bas123456789bas@hotmail.nl>
2021-01-15 04:53:40 +0100

    [*] Added second GetVectors function with Optix variables instead of GLM variables  to the Camera

--- Lumen_Engine/LumenPT/src/Framework/Camera.cpp
+++ Lumen_Engine/LumenPT/src/Framework/Camera.cpp
@@ -3,6 +3,8 @@
 #include <assert.h>
 
 #include <glm/gtx/rotate_vector.hpp>
+#include <Cuda/cuda/helpers.h>
+#include <Optix/optix_types.h>
 
 Camera::Camera() :
 	m_DirtyFlag(true)
@@ -65,6 +67,27 @@ void Camera::GetVectorData(glm::vec3& a_Eye, glm::vec3& a_U, glm::vec3& a_V, glm
 	a_V = m_Up * m_ImagePlaneHalfSize.y;
 
 	a_W = m_Forward * m_FocalLength;
+}
+
+void Camera::GetVectorData(float3& a_Eye, float3& a_U, float3& a_V, float3& a_W)
+{
+
+	if(m_DirtyFlag)
+	{
+		UpdateValues();
+	}
+
+	a_Eye = make_float3(m_Position.x, m_Position.y, m_Position.z);
+
+	glm::vec3 U = m_Right * m_ImagePlaneHalfSize.x;
+	a_U = make_float3(U.x, U.y, U.z);
+
+	glm::vec3 V = m_Up * m_ImagePlaneHalfSize.y;
+	a_V = make_float3(V.x, V.y, V.z);
+
+	glm::vec3 W = m_Forward * m_FocalLength;
+	a_W = make_float3(W.x, W.y, W.z);
+
 }
 
 void Camera::UpdateValues()
