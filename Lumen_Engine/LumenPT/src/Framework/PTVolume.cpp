bb90105af5364183209de1054f45e7009fba5ba5
Alex <alexandre.diallo@gmail.com>
2021-03-02 10:46:51 +0100

    [*] Small changes in PTVolume.cpp

--- Lumen_Engine/LumenPT/src/Framework/PTVolume.cpp
+++ Lumen_Engine/LumenPT/src/Framework/PTVolume.cpp
@@ -24,6 +24,13 @@ PTVolume::PTVolume(std::string a_FilePath, PTServiceLocator& a_ServiceLocator)
 	//TODO: remove
 	m_Handle = nanovdb::createLevelSetSphere<float, nanovdb::CudaDeviceBuffer>(10.0f, nanovdb::Vec3d(0), 0.1);
 	m_Handle.deviceUpload();
+
+	auto* grid = m_Handle.grid<float>();
+
+	auto voxelCount = grid->activeVoxelCount();
+
+	auto gridBBox = grid->indexBBox();
+	
 }
 
 PTVolume::~PTVolume()
