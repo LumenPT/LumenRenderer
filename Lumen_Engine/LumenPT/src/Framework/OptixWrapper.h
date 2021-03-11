cb6c01edd0d41712323ff5ca808a4fc2bbbda4c1
Bas Walhout <bas123456789bas@hotmail.nl>
2021-03-11 16:48:58 +0100

    [+] OptixWrapper class header with function declarations.

---
+++ Lumen_Engine/LumenPT/src/Framework/OptixWrapper.h
@@ -0,0 +1,141 @@
+#pragma once
+#ifdef WAVEFRONT
+#include <Lumen/Renderer/LumenRenderer.h>
+#include "../Shaders/CppCommon/WaveFrontDataStructs/OptixLaunchParams.h"
+#include "../Shaders/CppCommon/WaveFrontDataStructs/OptixShaderStructs.h"
+#include "ShaderBindingTableRecord.h"
+
+#include <Optix/optix.h>
+#include <filesystem>
+#include <memory>
+#include <vector>
+#include <map>
+
+class MemoryBuffer;
+class ShaderBindingTableGenerator;
+class AccelerationStructure;
+
+namespace WaveFront
+{
+
+    struct ComputedStackSizes
+    {
+        int DirectSizeTrace;
+        int DirectSizeState;
+        int ContinuationSize;
+    };
+
+    class OptixWrapper
+    {
+
+        struct InitializationData
+        {
+
+            CUcontext m_CUDAContext;
+
+            struct ProgramData
+            {
+                const std::filesystem::path m_ProgramPath;
+                const std::string& m_ProgramLaunchParamName;
+                const std::string& m_ProgramRayGenFuncName;
+                const std::string& m_ProgramMissFuncName;
+                const std::string& m_ProgramAnyHitFuncName;
+                const std::string& m_ProgramClosestHitFuncName;
+                uint8_t m_MaxNumPayloads = 2;
+                uint8_t m_MaxNumHitResultAttributes = 2;
+            }m_ProgramData;
+
+        };
+
+    public:
+
+        OptixWrapper(const InitializationData& a_InitializationData);
+        ~OptixWrapper();
+
+        template<typename VertexType, typename IndexType = uint32_t>
+        std::unique_ptr<AccelerationStructure> BuildGeometryAccelerationStructure(
+            std::vector<VertexType> a_Vertices, size_t a_VertexOffset = 0,
+            std::vector<IndexType> a_Indices = std::vector<IndexType>(), size_t a_IndexOffset = 0) const;
+
+        std::unique_ptr<AccelerationStructure> BuildGeometryAccelerationStructure(
+            const OptixAccelBuildOptions& a_BuildOptions,
+            const OptixBuildInput& a_BuildInput) const;
+
+        std::unique_ptr<AccelerationStructure> BuildInstanceAccelerationStructure(std::vector<OptixInstance> a_Instances) const;
+
+        void TraceRays(
+            RayType a_TraceType, 
+            const OptixLaunchParameters& a_LaunchParams,
+            float2 a_MinMaxTraceDistance);
+
+    private:
+
+        bool Initialize(const InitializationData& a_InitializationData);
+
+        bool InitializeContext(CUcontext a_CUDAContext);
+
+        bool OptixWrapper::CreatePipeline(const InitializationData::ProgramData& a_ProgramData);
+
+        OptixPipelineCompileOptions CreatePipelineOptions(
+            const std::string& a_LaunchParamName,
+            unsigned int a_NumPayloadValues,
+            unsigned int a_NumAttributes) const;
+
+        bool CreatePipeline(const OptixModule& a_Module,
+            const OptixPipelineCompileOptions& a_PipelineOptions,
+            const std::string& a_RayGenFuncName,
+            const std::string& a_MissFuncName,
+            const std::string& a_AnyHitFuncName,
+            const std::string& a_ClosestHitFuncName,
+            OptixPipeline& a_Pipeline);
+
+        OptixModule CreateModule(const std::filesystem::path& a_PtxPath, const OptixPipelineCompileOptions& a_PipelineOptions) const;
+
+        OptixProgramGroup CreateProgramGroup(OptixProgramGroupDesc a_ProgramGroupDesc, const std::string& a_Name);
+
+        ProgramGroupHeader GetProgramGroupHeader(const std::string& a_GroupName) const;
+
+        static void AccumulateStackSizes(OptixProgramGroup a_ProgramGroup, OptixStackSizes& a_StackSizes);
+
+        static ComputedStackSizes ComputeStackSizes(OptixStackSizes a_StackSizes, int a_TraceDepth, int a_DirectDepth, int a_ContinuationDepth);
+
+        void SetupPipelineBuffer();
+
+        void SetupShaderBindingTable();
+
+        static void OptixDebugCallback(unsigned int a_Level, const char* a_Tag, const char* a_Message, void*);
+
+
+
+        CUcontext m_CudaContex;
+
+        OptixDeviceContext m_DeviceContext;
+
+        OptixPipeline m_Pipeline;
+
+        OptixModule m_Module;
+
+        std::unique_ptr<ShaderBindingTableGenerator> m_SBTGenerator;
+
+        RecordHandle<OptixSBTRecordDataRayGen> m_RayGenRecord;
+        RecordHandle<OptixSBTRecordDataMiss> m_MissRecord;
+
+        std::unique_ptr<MemoryBuffer> m_SBTRecordBuffer;
+
+        std::unique_ptr<MemoryBuffer> m_OptixLaunchParameters;
+
+        std::map<std::string, OptixProgramGroup> m_ProgramGroups;
+
+        bool m_Initialized;
+
+        static constexpr char s_RayGenPGName[] = "RayGenPG";
+        static constexpr char s_HitPGName[] = "HitPG";
+        static constexpr char s_MissPGName[] = "MissPG";
+
+    };
+
+}
+
+#include "OptixWrapper.inl"
+
+#endif
