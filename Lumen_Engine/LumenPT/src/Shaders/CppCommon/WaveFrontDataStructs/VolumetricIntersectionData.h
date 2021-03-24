bb2866c128d04b2b19cd2fb97612e729c0173b48
Eli <snakfrien@gmail.com>
2021-03-24 13:33:55 +0100

    [+]Added volumetric intersection data struct

---
+++ Lumen_Engine/LumenPT/src/Shaders/CppCommon/WaveFrontDataStructs/VolumetricIntersectionData.h
@@ -0,0 +1,87 @@
+#pragma once
+#include "../CudaDefines.h"
+#include "../ModelStructs.h"
+#include "IntersectionRayBatch.h"
+
+#include <Optix/optix.h>
+#include <Cuda/cuda/helpers.h>
+#include <cassert>
+
+#include <nanovdb/NanoVDB.h>
+
+namespace WaveFront
+{
+	struct VolumetricIntersectionData
+	{
+		CPU_GPU VolumetricIntersectionData()
+			:
+			m_RayArrayIndex(0),
+			m_EntryT(-1.f),
+			m_ExitT(-1.f),
+			m_VolumeGrid(nullptr),
+			m_PixelIndex(0)
+		{
+		}
+
+		CPU_GPU VolumetricIntersectionData(
+			unsigned int a_RayArrayIndex,
+			float a_IntersectionT,
+			float a_EntryT,
+			float a_ExitT,
+			nanovdb::Grid<float>* a_VolumeGrid,
+			unsigned int a_PrimitiveIndex,
+			unsigned int a_InstanceId,
+			unsigned int a_PixelIndex)
+			:
+			m_RayArrayIndex(a_RayArrayIndex),
+			m_EntryT(a_EntryT),
+			m_ExitT(a_ExitT),
+			m_VolumeGrid(a_VolumeGrid),
+			m_PixelIndex(a_PixelIndex)
+		{}
+
+
+
+		/// <summary> Checks if the data defines an intersection. </summary>
+		/// <returns> Returns true if m_IntersectionT is higher than 0.  <em>(boolean)</em> </returns>
+		CPU_GPU INLINE bool IsIntersection() const
+		{
+			return (m_EntryT > 0.f);
+		}
+
+		/// <summary> Calculates the intersection point in world-space when given the right ray data as input.
+		/// If this intersection data is not an intersection, returns a 0 vector.
+		/// The calculation done is simply</summary>
+		/// <param name="a_RayData">\n • Description: A reference to the RayData struct used to construct the ray.</param>
+		/// <returns>Intersection in world-space if the intersection data is an intersection, else returns a 0 vector.</returns>
+		CPU_GPU INLINE float3 GetIntersectionPoint(const IntersectionRayData& a_RayData) const
+		{
+			if (IsIntersection())
+			{
+				return a_RayData.m_Origin + m_EntryT * a_RayData.m_Direction;
+			}
+			else return make_float3(0.f);
+		}
+
+
+
+		/// <summary>
+		/// <b>Description</b> \n The index in the m_Rays array of a RayBatch of the ray the intersection belongs to. \n
+		/// <b>Default</b>: 0
+		/// </summary>
+		unsigned int m_RayArrayIndex;
+		unsigned int m_RayArrayIndex;
+
+		//The index of the pixel/surface that this intersection affects.
+		unsigned int m_PixelIndex;
+
+		//Distance along ray to entry of volumetric bounding box
+		float m_EntryT;
+
+		//Distance along ray to exit of volumetric bounding box
+		float m_ExitT;
+
+		//Pointer to intersected volume as represented by a nanovdb grid of float type
+		nanovdb::Grid<float>* m_VolumeGrid;
+	};
+}
