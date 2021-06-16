2da7c0c95aa8391b077f144d0d3ed1934d5495fd
Alex <alexandre.diallo@gmail.com>
2021-06-16 10:46:55 +0200

    [*] Update Optix headers from 7.1 to 7.3

--- Lumen_Engine/LumenPT/src/Framework/OptixDenoiserWrapper.cpp
+++ Lumen_Engine/LumenPT/src/Framework/OptixDenoiserWrapper.cpp
@@ -13,70 +13,70 @@ OptixDenoiserWrapper::~OptixDenoiserWrapper()
 
 void OptixDenoiserWrapper::Initialize(const OptixDenoiserInitParams& a_InitParams)
 {
-    assert(a_InitParams.m_ServiceLocator != nullptr);
-    assert(a_InitParams.m_ServiceLocator->m_OptixWrapper != nullptr);
+    //assert(a_InitParams.m_ServiceLocator != nullptr);
+    //assert(a_InitParams.m_ServiceLocator->m_OptixWrapper != nullptr);
 
-    m_InitParams = a_InitParams;
-    WaveFront::OptixWrapper& optixWrapper = *(m_InitParams.m_ServiceLocator->m_OptixWrapper);
+    //m_InitParams = a_InitParams;
+    //WaveFront::OptixWrapper& optixWrapper = *(m_InitParams.m_ServiceLocator->m_OptixWrapper);
 
-    OptixDenoiserOptions options = {}; //this struct is missing some options?
-    options.inputKind = OPTIX_DENOISER_INPUT_RGB; //TODO: add albedo and normal at later date
-    
-    CHECKOPTIXRESULT(optixDenoiserCreate(optixWrapper.GetDeviceContext(), &options, &m_Denoiser));
+    //OptixDenoiserOptions options = {}; //this struct is missing some options?
+    //options.inputKind = OPTIX_DENOISER_INPUT_RGB; //TODO: add albedo and normal at later date
+    //
+    //CHECKOPTIXRESULT(optixDenoiserCreate(optixWrapper.GetDeviceContext(), &options, &m_Denoiser));
 
-    OptixDenoiserModelKind denoiserModelKind = {};
-    denoiserModelKind = OPTIX_DENOISER_MODEL_KIND_LDR;
+    //OptixDenoiserModelKind denoiserModelKind = {};
+    //denoiserModelKind = OPTIX_DENOISER_MODEL_KIND_LDR;
 
-    CHECKOPTIXRESULT(optixDenoiserSetModel(m_Denoiser, denoiserModelKind, nullptr, 0));
+    //CHECKOPTIXRESULT(optixDenoiserSetModel(m_Denoiser, denoiserModelKind, nullptr, 0));
 
-    OptixDenoiserSizes denoiserSizes = {};
-    CHECKOPTIXRESULT(optixDenoiserComputeMemoryResources(m_Denoiser, m_InitParams.m_InputWidth, m_InitParams.m_InputHeight, &denoiserSizes)); //TODO: might have to set max input to 4K resolution
+    //OptixDenoiserSizes denoiserSizes = {};
+    //CHECKOPTIXRESULT(optixDenoiserComputeMemoryResources(m_Denoiser, m_InitParams.m_InputWidth, m_InitParams.m_InputHeight, &denoiserSizes)); //TODO: might have to set max input to 4K resolution
 
-    m_state.Resize(denoiserSizes.stateSizeInBytes);
-    m_scratch.Resize(denoiserSizes.withoutOverlapScratchSizeInBytes);
+    //m_state.Resize(denoiserSizes.stateSizeInBytes);
+    //m_scratch.Resize(denoiserSizes.withoutOverlapScratchSizeInBytes);
 
-    CHECKOPTIXRESULT(optixDenoiserSetup(
-        m_Denoiser,
-        0,
-        m_InitParams.m_InputWidth,
-        m_InitParams.m_InputHeight,
-        static_cast<CUdeviceptr>(m_state.GetCUDAPtr()),
-        m_state.GetSize(),
-        static_cast<CUdeviceptr>(m_scratch.GetCUDAPtr()),
-        m_scratch.GetSize()
-    ));
+    //CHECKOPTIXRESULT(optixDenoiserSetup(
+    //    m_Denoiser,
+    //    0,
+    //    m_InitParams.m_InputWidth,
+    //    m_InitParams.m_InputHeight,
+    //    static_cast<CUdeviceptr>(m_state.GetCUDAPtr()),
+    //    m_state.GetSize(),
+    //    static_cast<CUdeviceptr>(m_scratch.GetCUDAPtr()),
+    //    m_scratch.GetSize()
+    //));
 
-    TestInput.Resize(m_InitParams.m_InputWidth * m_InitParams.m_InputHeight * sizeof(float3));
-    TestOutput.Resize(m_InitParams.m_InputWidth * m_InitParams.m_InputHeight * sizeof(float3));
+    //TestInput.Resize(m_InitParams.m_InputWidth * m_InitParams.m_InputHeight * sizeof(float3));
+    //TestOutput.Resize(m_InitParams.m_InputWidth * m_InitParams.m_InputHeight * sizeof(float3));
 }
 
 void OptixDenoiserWrapper::Denoise(const OptixDenoiserDenoiseParams& a_DenoiseParams)
 {
-    assert(a_DenoiseParams.m_PostProcessLaunchParams != nullptr);
+    //assert(a_DenoiseParams.m_PostProcessLaunchParams != nullptr);
 
-    OptixDenoiserParams optixDenoiserParams = {};
-    optixDenoiserParams.denoiseAlpha = false;
-    optixDenoiserParams.blendFactor = 0.0f;
+    //OptixDenoiserParams optixDenoiserParams = {};
+    //optixDenoiserParams.denoiseAlpha = false;
+    //optixDenoiserParams.blendFactor = 0.0f;
 
-    //MemoryBuffer test1(m_InitParams.m_InputWidth * m_InitParams.m_InputHeight * sizeof(float3));
-    //MemoryBuffer test2(m_InitParams.m_InputWidth * m_InitParams.m_InputHeight * sizeof(float3));
+    ////MemoryBuffer test1(m_InitParams.m_InputWidth * m_InitParams.m_InputHeight * sizeof(float3));
+    ////MemoryBuffer test2(m_InitParams.m_InputWidth * m_InitParams.m_InputHeight * sizeof(float3));
 
-    std::vector<OptixImage2D> inputLayers;
-    OptixImage2D& colorTex = inputLayers.emplace_back();
-    colorTex.data = /*static_cast<CUdeviceptr>(test1.GetCUDAPtr())*/a_DenoiseParams.m_ColorInput;
-    colorTex.width = m_InitParams.m_InputWidth;
-    colorTex.height = m_InitParams.m_InputHeight;
-    colorTex.pixelStrideInBytes = sizeof(float3);
-    colorTex.rowStrideInBytes = colorTex.pixelStrideInBytes * colorTex.width;
-    colorTex.format = OPTIX_PIXEL_FORMAT_FLOAT3;
+    //std::vector<OptixImage2D> inputLayers;
+    //OptixImage2D& colorTex = inputLayers.emplace_back();
+    //colorTex.data = /*static_cast<CUdeviceptr>(test1.GetCUDAPtr())*/a_DenoiseParams.m_ColorInput;
+    //colorTex.width = m_InitParams.m_InputWidth;
+    //colorTex.height = m_InitParams.m_InputHeight;
+    //colorTex.pixelStrideInBytes = sizeof(float3);
+    //colorTex.rowStrideInBytes = colorTex.pixelStrideInBytes * colorTex.width;
+    //colorTex.format = OPTIX_PIXEL_FORMAT_FLOAT3;
 
-    OptixImage2D outputTex;
-    outputTex.data = /*static_cast<CUdeviceptr>(test2.GetCUDAPtr())*/a_DenoiseParams.m_Output;
-    outputTex.width = m_InitParams.m_InputWidth;
-    outputTex.height = m_InitParams.m_InputHeight;
-    outputTex.pixelStrideInBytes = sizeof(float3);
-    colorTex.rowStrideInBytes = colorTex.pixelStrideInBytes * colorTex.width;
-    outputTex.format = OPTIX_PIXEL_FORMAT_FLOAT3;
+    //OptixImage2D outputTex;
+    //outputTex.data = /*static_cast<CUdeviceptr>(test2.GetCUDAPtr())*/a_DenoiseParams.m_Output;
+    //outputTex.width = m_InitParams.m_InputWidth;
+    //outputTex.height = m_InitParams.m_InputHeight;
+    //outputTex.pixelStrideInBytes = sizeof(float3);
+    //colorTex.rowStrideInBytes = colorTex.pixelStrideInBytes * colorTex.width;
+    //outputTex.format = OPTIX_PIXEL_FORMAT_FLOAT3;
 
     /*CHECKLASTCUDAERROR;
     auto result = optixDenoiserInvoke(
