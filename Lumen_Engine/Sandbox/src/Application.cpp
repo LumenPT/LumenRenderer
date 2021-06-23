de644244184672e9f93fe0e64e7d65864e211659
Jan <janie177@hotmail.com>
2021-06-23 14:36:05 +0200

    Add profiling to the profiling output buffer.
    [+]
    Profiling statistics for Wavefront.
    Comment out some debug code in release mode.

--- Lumen_Engine/Sandbox/src/Application.cpp
+++ Lumen_Engine/Sandbox/src/Application.cpp
@@ -94,9 +94,9 @@ public:
 		settings.depth = 5;
 		settings.minIntersectionT = 0.1f;
 		settings.maxIntersectionT = 5000.f;
-		settings.renderResolution = { 1280, 720 };
+		settings.renderResolution = { 960, 540 };
 		//settings.outputResolution = { 2560, 1440 };
-		settings.outputResolution = { 1280, 720 };
+		settings.outputResolution = { 960, 540 };
 		settings.blendOutput = false;	//When true will blend output instead of overwriting it (high res image over time if static scene).
 
 		std::static_pointer_cast<WaveFront::WaveFrontRenderer>(renderer)->Init(settings);
